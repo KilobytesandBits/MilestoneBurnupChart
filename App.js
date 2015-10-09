b68ae10095fb9a472fc9ca3911e8b6333a4f74a6
suvonkar.dam <suvonkar.dam@lexmark.com>
2015-10-09 17:08:43 +0530

    US84727 - Milestone Picker is implemented

--- App.js
+++ App.js
@@ -1,26 +1,72 @@
 Ext.define('CustomApp', {
 	extend : 'Rally.app.App',
 	componentCls : 'app',
+
+	getSettingsFields : function() {
+		return [ {
+			name : 'IncludeNullTargetProjMilestone',
+			xtype : 'rallycheckboxfield',
+			fieldLabel : '',
+			boxLabel : 'Include Milestones with All Projects in Research and Development Workspace'
+		} ];
+	},
+
 	launch : function() {
 		this._createMilestoneWaspiDataStore();
 	},
 
+	/**
+	 * Create the WASPI Data Store for Milestone
+	 */
 	_createMilestoneWaspiDataStore : function() {
 		Ext.getBody().mask('Loading...');
+		console.log("Rally.environment.getContext().getProject()._ref : ", Rally.environment.getContext().getProject()._ref);
+
+		//Create filter based on settings selection
+		var filter;
+		if (this.getSetting('IncludeNullTargetProjMilestone')) {
+			var projFilter = Ext.create('Rally.data.wsapi.Filter', {
+				property : 'TargetProject',
+				operator : '=',
+				value : Rally.environment.getContext().getProject()._ref
+			});
+			filter = projFilter.or(Ext.create('Rally.data.wsapi.Filter', {
+				property : 'TargetProject',
+				operator : '=',
+				value : null
+			}));
+		} else {
+			filter = Ext.create('Rally.data.wsapi.Filter', {
+				property : 'TargetProject',
+				operator : '=',
+				value : Rally.environment.getContext().getProject()._ref
+			});
+		}
+
 		milestoneWaspiDataStore = Ext.create('Rally.data.wsapi.Store', {
 			model : 'Milestone',
 			autoLoad : true,
+			compact : false,
 			context : {
-				project : Rally.environment.getContext().getProject(),
+				workspace : Rally.environment.getContext().getWorkspace()._ref,
+				project : Rally.environment.getContext().getProject()._ref,
 				projectScopeUp : false,
 				projectScopeDown : true
 			},
-			fetch : [ 'Name', 'TargetDate', 'TargetProject' ],
+			filters : filter,
+			fetch : [ 'FormattedID', 'Name', 'TargetDate', 'TargetProject' ],
 			limit : Infinity,
 			listeners : {
 				load : function(store, data, success) {
-					console.log("Data : ", data);
-					this._createMilestoneDataStore(data);
+					if (data.length > 0) {
+						this._createMilestoneDataStore(data);
+					} else {
+						//Ext.Msg.alert('Warning', 'No Milestone is associated with the selected Project.');
+						Rally.ui.notify.Notifier.showError({
+							message : 'No Milestone is associated with the selected Project.'
+						});
+					}
+					Ext.getBody().unmask();
 				},
 				scope : this
 			},
@@ -31,47 +77,61 @@ Ext.define('CustomApp', {
 		});
 	},
 
+	/**
+	 * Convert the WASPI Data Store for Milestone to Ext.data.Store
+	 */
 	_createMilestoneDataStore : function(myData) {
 
 		var milestoneArr = [];
 
 		Ext.each(myData, function(data, index) {
 			var milestone = {};
+			milestone.FormattedID = data.data.FormattedID;
 			milestone.Name = data.data.Name;
 			milestone.TargetDate = data.data.TargetDate;
 			milestone.TargetProject = data.data.TargetProject;
 			milestoneArr.push(milestone);
 		});
 
-		console.log("milestoneArr : ", milestoneArr);
-
 		this.milestoneDataStore = Ext.create('Ext.data.Store', {
-			fields : [ 'Name', 'TargetDate', 'TargetProject' ],
+			fields : [ 'FormattedID', 'Name', 'TargetDate', 'TargetProject' ],
 			data : milestoneArr
 		});
 		this._createMilestonePicker();
 	},
 
+	/**
+	 * Create the Ext.form.ComboBox for the Milestone
+	 */
 	_createMilestonePicker : function() {
 		this.milestonePicker = Ext.create('Ext.form.ComboBox', {
-			fieldLabel : 'Milestone : ',
+			fieldLabel : 'Milestone ',
 			store : this.milestoneDataStore,
 			renderTo : Ext.getBody(),
 			displayField : 'Name',
 			queryMode : 'local',
-			valueField : 'Name',
+			valueField : 'FormattedID',
 			border : 1,
 			style : {
 				borderColor : '#000000',
 				borderStyle : 'solid',
-				borderWidth : '1px'
+				borderWidth : '1px',
+				height : '40px'
 			},
-			width: 400,
-			padding: '10 5 5 10',
-			margin: '10 5 5 10'
+			width : 400,
+			padding : '10 5 5 10',
+			margin : '10 5 5 10',
+			shadow : 'frame',
+			labelAlign : 'right',
+			labelStyle : {
+				margin : '10 5 5 10'
+			},
+			listeners : {
+				select : function(combo, records, eOpts) {
+					console.log(combo.getValue());
+				}
+			}
 		});
 		this.add(this.milestonePicker);
-		Ext.getBody().unmask();
 	}
-
 });
